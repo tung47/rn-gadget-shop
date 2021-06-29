3c021e4f08b3b8c08db1374c9b9f77c96df64ddb
tung47 <tung.nguyen271193@gmail.com>
2021-06-29 15:51:03 +0300

    products overview screen

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PlatList } from 'react-native';
+
+const ProductOverviewScreen = (props) => {
+  return <PlatList />;
+};
+
+export default ProductOverviewScreen;
