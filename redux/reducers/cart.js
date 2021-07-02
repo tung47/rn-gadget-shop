707f6808c323d4b320a53192c31d65414b1fbf74
tung47 <tung.nguyen271193@gmail.com>
2021-07-02 12:07:13 +0300

    add item to cart

---
+++ redux/reducers/cart.js
@@ -0,0 +1,42 @@
+import { ADD_TO_CART } from '../actions/cart';
+import CartItem from '../../models/cart-item';
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const productPrice = addedProduct.price;
+      const productTitle = addedProduct.title;
+
+      let updatedOrNewCartItem;
+
+      if (state.items[addedProduct.id]) {
+        // item in cart already
+        updatedOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          productPrice,
+          productTitle,
+          state.items[addedProduct.id].sum + productPrice
+        );
+      } else {
+        // new item added
+        updatedOrNewCartItem = new CartItem(
+          1,
+          productPrice,
+          productTitle,
+          productPrice
+        );
+      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+        totalAmount: state.totalAmount + productPrice,
+      };
+  }
+  return state;
+};
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,21 +7,29 @@ import {
   Button,
   StyleSheet,
 } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
+import * as cartActions from '../../redux/actions/cart';
 
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam('productId');
   const selectedProduct = useSelector((state) =>
     state.products.availableProducts.find((product) => product.id === productId)
   );
+  const dispatch = useDispatch();
 
   return (
     <ScrollView>
       <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
       <View style={styles.actions}>
-        <Button color={Colors.accent} title='Add to Cart' onPress={() => {}} />
+        <Button
+          color={Colors.accent}
+          title='Add to Cart'
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct));
+          }}
+        />
       </View>
       <Text style={styles.price}>€{selectedProduct.price.toFixed(2)}</Text>
       <Text style={styles.description}>{selectedProduct.description}</Text>
