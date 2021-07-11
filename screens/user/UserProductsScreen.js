e8fa72e6c81cbbeb9a70e3287b9291bc7fa01a8a
tung47 <tung.nguyen271193@gmail.com>
2021-07-11 11:19:55 +0300

    create user products screen

---
+++ screens/user/UserProductsScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FlatList, Platform } from 'react-native';
+import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import ProductItem from '../../components/shop/ProductItem';
+import CustomHeaderButton from '../../components/UI/HeaderButton';
+
+const UserProductsScreen = (props) => {
+  const userProducts = useSelector((state) => state.products.userProducts);
+
+  return (
+    <FlatList
+      data={userProducts}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+UserProductsScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: 'Your Products',
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title='Menu'
+          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+export default UserProductsScreen;
