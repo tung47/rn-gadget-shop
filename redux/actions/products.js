18d4b3d216c111742a0ce0f450e7beb1e11b5518
tung47 <tung.nguyen271193@gmail.com>
2021-07-27 23:58:57 +0300

    add loading spinner and error handler

--- redux/actions/products.js
+++ redux/actions/products.js
@@ -1,6 +1,5 @@
 import Product from '../../models/product';
 
-
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
@@ -8,27 +7,36 @@ export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 
 export const fetchProducts = () => {
   return async (dispatch) => {
-    const response = await fetch(
-      'https://rn-gadget-shop-default-rtdb.europe-west1.firebasedatabase.app/products.json'
-    );
-
-    const resData = await response.json();
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          'u1',
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        'https://rn-gadget-shop-default-rtdb.europe-west1.firebasedatabase.app/products.json'
       );
-    }
 
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
+    }
   };
 };
 
