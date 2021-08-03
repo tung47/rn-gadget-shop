3666588269eea62bb7111aaa8d022657d73f7d8c
tung47 <tung.nguyen271193@gmail.com>
2021-08-03 23:59:43 +0300

    add order action

--- redux/actions/orders.js
+++ redux/actions/orders.js
@@ -2,6 +2,23 @@ export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
+    const response = await fetch(
+      'https://rn-gadget-shop-default-rtdb.europe-west1.firebasedatabase.app/orders.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title, description, imageUrl, price }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+
     dispatch({
       type: ADD_ORDER,
       orderData: { items: cartItems, amount: totalAmount },
