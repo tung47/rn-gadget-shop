b6f5c867dfb964768380ad25d9b9223259cd397e
tung47 <tung.nguyen271193@gmail.com>
2021-08-03 23:57:24 +0300

    add orders to database

--- redux/actions/orders.js
+++ redux/actions/orders.js
@@ -1,8 +1,10 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount },
+  return async (dispatch) => {
+    dispatch({
+      type: ADD_ORDER,
+      orderData: { items: cartItems, amount: totalAmount },
+    });
   };
 };
