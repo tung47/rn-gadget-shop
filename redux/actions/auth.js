6db79b0e8236ee998c943385114499b575f7e63d
tung47 <tung.nguyen271193@gmail.com>
2021-08-10 00:20:35 +0300

    add user sign up

--- redux/actions/auth.js
+++ redux/actions/auth.js
@@ -3,23 +3,24 @@ export const SIGNUP = 'SIGNUP';
 export const signup = (email, password) => {
   return async (dispatch) => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAtLHYSTh70paVx7qntrMyq2iWs5pg4H5g', { 
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAtLHYSTh70paVx7qntrMyq2iWs5pg4H5g',
+      {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json'}
-      },
-      body: JSON.stringify({
-        email:email,
-        password:password,
-        returnSecureToken: true,
-      })
-  );
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
 
     if (!response.ok) {
       throw new Error('Something went wrong!');
     }
 
     const resData = await response.json();
-    console.log(resData)
+    console.log(resData);
     dispatch({ type: SIGNUP });
   };
 };
