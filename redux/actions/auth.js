7648d847b9d3a0b57f74ff9369a0b79e533e1965
tung47 <tung.nguyen271193@gmail.com>
2021-08-09 23:59:22 +0300

    auth action

---
+++ redux/actions/auth.js
@@ -0,0 +1,25 @@
+export const SIGNUP = 'SIGNUP';
+
+export const signup = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAtLHYSTh70paVx7qntrMyq2iWs5pg4H5g', { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'}
+      },
+      body: JSON.stringify({
+        email:email,
+        password:password,
+        returnSecureToken: true,
+      })
+  );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    console.log(resData)
+    dispatch({ type: SIGNUP });
+  };
+};
